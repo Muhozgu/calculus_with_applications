84794aafbccaa8f992fd6d40aaf89cd0e8331415
Guy Muhoza <muhozgu_aehit@students.vizja.pl>
2026-02-05 14:44:26 +0100

    Findind an equation of a line given two points

--- exercises/slope/equation.py
+++ exercises/slope/equation.py
@@ -1,6 +1,16 @@
 # Finding an aquation of a  line given a point and a slope
+from slopes import slope
+
+
 
 def find_d(m, x1, y1):
     c = -m*x1 + y1
     d = f'{m}x'
     return f'y = {d} + {c}'
+
+
+def find_d2p(x1, y1, x2, y2):
+    m = slope(x1, y1, x2, y2)
+    return find_d(m, x1, y1)
+
+print(find_d2p(-1, 3, 0, 3))
